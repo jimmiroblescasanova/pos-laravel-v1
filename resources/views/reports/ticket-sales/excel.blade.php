80423e0d1a25d3f490252b7b431a71b43ae70b66
Jimmi Robles <jimmirobles@icloud.com>
2023-09-27 14:32:42 -0500

    Update excel.blade.php
    Actualizacion del reporte

--- resources/views/reports/ticket-sales/excel.blade.php
+++ resources/views/reports/ticket-sales/excel.blade.php
@@ -16,42 +16,63 @@
     </table>
     <table>
         <thead>
-            <tr style="font-weight: bold">
+            <tr style="font-weight: bold;">
                 <th>Fecha</th>
                 <th>Folio</th>
                 <th>Cliente</th>
+                <th>Precio U.</th>
+                <th>Importe</th>
                 <th>Descuento</th>
                 <th>Impuesto</th>
                 <th>Total</th>
+                <th>Forma de Pago</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($orders as $order)
-                <tr>
-                    <td>{{ $order->updated_at->format('Y-m-d') }}</td>
-                    <td>{{ $order->id }}</td>
-                    <td>{{ $order->customer }}</td>
-                    <td>{{ $order->discount }}</td>
-                    <td>{{ $order->tax }}</td>
-                    <td>{{ $order->total }}</td>
+                <tr style="border: 1px solid #000;">
+                    <td style="border-top: 1px solid black">{{ $order->updated_at->format('d-m-Y') }}</td>
+                    <td style="border-top: 1px solid black">{{ $order->id }}</td>
+                    <td style="border-top: 1px solid black">{{ $order->customer }}</td>
+                    <td style="border-top: 1px solid black"></td>
+                    <td style="border-top: 1px solid black"></td>
+                    <td style="border-top: 1px solid black">{{ $order->discount }}</td>
+                    <td style="border-top: 1px solid black">{{ $order->tax }}</td>
+                    <td style="border-top: 1px solid black">{{ $order->total }}</td>
+                    <td style="border-top: 1px solid black">{{ paymentMethod($order->payment_method) }}</td>
                 </tr>
-                <tr>
-                    <td></td>
-                    <td>Cantidad</td>
-                    <td>Producto</td>
-                    <td>Precio</td>
-                </tr>
-                @foreach ($order->items as $item)
+                @foreach ($order->items as $key => $item)
                     <tr>
                         <td></td>
                         <td>{{ $item->quantity }}</td>
                         <td>{{ $item->product->name }}</td>
                         <td>{{ $item->price }}</td>
+                        <td>{{ $item->quantity * $item->price }}</td>
                     </tr>
                 @endforeach
-                <tr></tr>
-                <tr></tr>
             @endforeach
+            @foreach($orders->groupBy('payment_method') as $pay => $subOrders)
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td style="font-size: 16px; font-weight: bold;">{{ paymentMethod($pay) }}</td>
+                <td style="font-size: 16px; font-weight: bold;">{{ number_format($subOrders->sum('total'), 2) }}</td>
+            </tr>
+            @endforeach
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td style="font-size: 16px; font-weight: bold;">TOTAL GENERAL DEL DÍA</td>
+                <td style="font-size: 16px; font-weight: bold;">{{ number_format($orders->sum('total'), 2) }}</td>
+            </tr>
         </tbody>
     </table>
 </body>
