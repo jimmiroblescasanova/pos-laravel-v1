d443449dd7d394ad77b76591994070189df41b17
Jimmi Robles <jimmirobles@icloud.com>
2024-03-27 12:44:45 -0500

    added option to check table existence on service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,38 +33,40 @@ class AppServiceProvider extends ServiceProvider
         Carbon::setLocale($this->app->getLocale());
         setlocale(LC_TIME, $this->app->getLocale());
 
-        // Cache daily sales
-        $dailySales = cache()->remember('dailySales', 300, function () {
-            return Order::query()
-                ->where('closed', 1)
-                ->whereDate('updated_at', Carbon::today())
-                ->sum('total');
-        });
-        View::share('dailySales', $dailySales);
+        if (Schema::hasTable('order')) {
+            // Cache daily sales
+            $dailySales = cache()->remember('dailySales', 300, function () {
+                return Order::query()
+                    ->where('closed', 1)
+                    ->whereDate('updated_at', Carbon::today())
+                    ->sum('total');
+            });
+            View::share('dailySales', $dailySales);
 
-        // Cache history sales
-        $historySales = cache()->remember('historySales', 3600, function () {
-            return Order::query()
-                ->select(DB::raw("SUM(total/100) sales"), DB::raw("DATE_FORMAT(updated_at, '%Y %m') as months"))
-                ->onlyClosed()
-                ->orderBy('months', 'desc')
-                ->groupBy('months')
-                ->take(5)
-                ->get()
-                ->pluck('sales', 'months');
-        });
-        View::share('historySales', $historySales);
+            // Cache history sales
+            $historySales = cache()->remember('historySales', 3600, function () {
+                return Order::query()
+                    ->select(DB::raw("SUM(total/100) sales"), DB::raw("DATE_FORMAT(updated_at, '%Y %m') as months"))
+                    ->onlyClosed()
+                    ->orderBy('months', 'desc')
+                    ->groupBy('months')
+                    ->take(5)
+                    ->get()
+                    ->pluck('sales', 'months');
+            });
+            View::share('historySales', $historySales);
 
-        // Cache sales by product
-        $top5products = cache()->remember('top5products', 3600, function () {
-            return Product::query()
-                ->where('active', true)
-                ->orderBy('total_sales', 'desc')
-                ->limit(5)
-                ->get()
-                ->pluck('total_sales', 'name');
-        });
-        View::share('top5products', $top5products);
+            // Cache sales by product
+            $top5products = cache()->remember('top5products', 3600, function () {
+                return Product::query()
+                    ->where('active', true)
+                    ->orderBy('total_sales', 'desc')
+                    ->limit(5)
+                    ->get()
+                    ->pluck('total_sales', 'name');
+            });
+            View::share('top5products', $top5products);
+        }
 
     }
 }
